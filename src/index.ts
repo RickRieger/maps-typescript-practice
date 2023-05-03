e0535581f4a05ad93a0935ab61a98df799cfb988
Rick Rieger <rickrieger36251@gmail.com>
2023-05-03 00:32:16 -0400

    brought in the google map to index.js

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 /// <reference types="@types/google.maps" />
-
 import { User } from "./User";
 import { Company } from "./Company";
 const user = new User();
 const company = new Company();
-console.log("===>", user);
-console.log("===>", company);
+document.getElementById("map");
+
+let div = document.getElementById("map") as HTMLElement;
+
+new google.maps.Map(div, {
+  zoom: 1,
+  center: {
+    lat: 0,
+    lng: 0,
+  },
+});
