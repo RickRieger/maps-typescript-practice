c8e34f10099753f8a79ffc50096d78ff4f8333f8
Rick Rieger <rickrieger36251@gmail.com>
2023-05-03 09:24:51 -0400

    created private custom map class

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,7 @@
 /// <reference types="@types/google.maps" />
 import { User } from "./User";
 import { Company } from "./Company";
+import { CustomMap } from "./CustomMap";
 const user = new User();
 const company = new Company();
-document.getElementById("map");
-
-let div = document.getElementById("map") as HTMLElement;
-
-new google.maps.Map(div, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0,
-  },
-});
+const map = new CustomMap();
